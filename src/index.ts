2bbdf23ab6278a73448b4ff1b735143afa25c3c5
Edson Felipe <edson.felipe@onlypay.com.br>
2022-04-07 20:29:26 -0300

    Desafio 3 done.

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { maxHeaderSize } from "http";
 
 dotenv.config();
 
@@ -33,11 +34,51 @@ app.use(express.json());
  * Server Activation
  */
 
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+app.listen(PORT, async () => {
+	console.log(`Listening on port ${PORT}`)
 
-
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
+		// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
+
+const readline = require('readline');
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+	function askQuestion(query) {
+
+		return new Promise(resolve => rl.question(query, ans => {
+			resolve(ans);
+		}))
+	}
 	
+	 async function addNewPessoa() {
+		var pessoaNova = {};
+	
+		pessoaNova.nome = await askQuestion("Qual o nome do aluno a ser registrado?");
+		pessoaNova.nota = await askQuestion("Qual a nota do aluno a ser registrado?");
+		return pessoaNova;
+		}
+
+	var numeroDeAlunos = 0;
+	var listaDePessoas = [];
+
+	numeroDeAlunos = await askQuestion("Qual o numero de estudantes a registrar?")
+
+	for (let i = 0; i < numeroDeAlunos; i++) {
+		listaDePessoas.push(await addNewPessoa());
+
+	}
+	console.table(listaDePessoas)
+	let max = listaDePessoas[0]
+
+	for (var i = 0; i < listaDePessoas.length; i++){
+		if(listaDePessoas[i].nota >= max.nota){
+			max = listaDePessoas[i];
+			console.log(listaDePessoas[i] + " >= " +max)
+		}
+	}
+	console.log("O aluno com a maior nota foi:")
+	console.log(max)
 });
